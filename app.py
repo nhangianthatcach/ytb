500619616ebed9f8ab4441c79a5b5c4f9fc25a02
KMH <kmhtheking2@gmail.com>
2026-07-15 11:18:54 +0700

    Cập nhật app.py

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ HTML_TEMPLATE = '''
 <html lang="vi">
 <head>
     <meta charset="UTF-8">
-    <title>Tool Lấy Data YouTube</title>
+    <title>Lấy dữ liệu UTB</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
     <div class="container mt-5">
         <div class="card shadow-sm p-4">
-            <h2 class="text-center mb-4 text-primary">NẠP DATA YOUTUBE</h2>
+            <h2 class="text-center mb-4 text-primary">TEST_KMH</h2>
             <form method="POST">
                 <div class="mb-3">
                     <label class="form-label fw-bold">Link YouTube (Video/Short):</label>
